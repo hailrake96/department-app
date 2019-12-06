5bb7acc4c868b0718c2527c204912a3cf2cfd8e2
AKruKovych <anatoliinakrukovich@gmail.com>
2019-12-06 23:10:15 +0200

    migrations added

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate
 
 # local imports
 from config import app_config
@@ -16,15 +17,17 @@ login_manager = LoginManager()
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
+    # app.config.from_object(app_config[config_name])
+    # app.config.from_pyfile('/home/anatolii/department-app/app/instance/config.py')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Ak12345678@localhost/departments_db'
     db.init_app(app)
     login_manager.init_app(app)
     login_manager.login_message = "You must be logged in to access this page."
     login_manager.login_view = "auth.login"
-    
+    migrate = Migrate(app, db)
+
     @app.route('/')
-    def hello_world():
+    def hello_worl1():
         return 'Hello,World'
 
     return app
