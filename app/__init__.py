882446ce798b8cb238aaab13831ad95c551e7f7d
AKruKovych <anatoliinakrukovich@gmail.com>
2019-12-08 16:36:58 +0200

    created auth forms and views

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
 
 # local imports
 from config import app_config
@@ -18,18 +19,30 @@ login_manager = LoginManager()
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
-    # app.config.from_object(app_config[config_name])
+    app.config.from_object(app_config[config_name])
     # app.config.from_pyfile('/home/anatolii/department-app/app/instance/config.py')
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Ak12345678@localhost/dep_db'
+
+    Bootstrap(app)
     db.init_app(app)
     login_manager.init_app(app)
     login_manager.login_message = "You must be logged in to access this page."
     login_manager.login_view = "auth.login"
     migrate = Migrate(app, db)
+
     from app import models
 
-    @app.route('/')
-    def hello_worl1():
-        return 'Hello,World'
+    # Each blueprint object has imported  and registered. For the admin blueprint,
+    # url prefix /admin is added. This means that all the views for this blueprint will be accessed
+    # in the browser with the url prefix admin.
+
+    from app.admin import admin as admin_blueprint
+    app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+    #
+    from .home import home as home_blueprint
+    app.register_blueprint(home_blueprint)
 
     return app
