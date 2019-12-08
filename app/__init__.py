c88cb40268f0dd86bad97fc698da209c5d3e2621
AKruKovych <anatoliinakrukovich@gmail.com>
2019-12-08 21:33:08 +0200

    last commit today. flask db migrate does not refresh version and db

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_migrate import Migrate
+from flask_migrate import Migrate,MigrateCommand
 from flask_bootstrap import Bootstrap
+from flask_script import Manager
 
 # local imports
 from config import app_config
@@ -19,16 +20,19 @@ login_manager = LoginManager()
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
+    # app.config.from_object(app_config[config_name])
     app.config.from_pyfile('/home/anatolii/department-app/app/instance/config.py')
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Ak12345678@localhost/dep_db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Ak12345678@localhost/dep_db'
 
     Bootstrap(app)
     db.init_app(app)
     login_manager.init_app(app)
     login_manager.login_message = "You must be logged in to access this page."
     login_manager.login_view = "auth.login"
+
     migrate = Migrate(app, db)
+    manager = Manager(app)
+    manager.add_command('db',MigrateCommand,compare_type=True)
 
     from app import models
 
