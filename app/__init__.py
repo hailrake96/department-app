895948b8d2359de8e34e41318c4bc7725387e5de
AKruKovych <anatoliinakrukovich@gmail.com>
2019-12-14 18:08:29 +0200

    Trying to set up travis-ci

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 # app/__init__.py
 
 # third-party imports
-from flask import Flask
+from flask import Flask, render_template, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_migrate import Migrate,MigrateCommand
+from flask_migrate import Migrate, MigrateCommand
 from flask_bootstrap import Bootstrap
 from flask_script import Manager
 
@@ -32,7 +32,7 @@ def create_app(config_name):
 
     migrate = Migrate(app, db)
     manager = Manager(app)
-    manager.add_command('db',MigrateCommand,compare_type=True)
+    manager.add_command('db', MigrateCommand, compare_type=True)
 
     from app import models
 
@@ -45,8 +45,24 @@ def create_app(config_name):
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
-    #
+
     from .home import home as home_blueprint
     app.register_blueprint(home_blueprint)
+
+    @app.errorhandler(403)
+    def forbidden(error):
+        return render_template('errors/403.html', title='Forbidden'), 403
+
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template('errors/404.html', title='Page Not Found'), 404
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return render_template('errors/500.html', title='Server Error'), 500
+
+    @app.route('/500')
+    def error():
+        abort(500)
 
     return app
